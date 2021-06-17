8a137f04ce93d48b4dfde07b25955a557489407e
Hye Ri Kang <estherhrkang@gmail.com>
2021-06-17 15:55:10 -0400

    set up user login api route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,8 +1,33 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
 // Login: POST /api/session
+router.post('/', asyncHandler( async(req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login({ credential, password });
+
+    if (!user) {
+        const err = new Error('Login Failed');
+        err.status = 401;
+        err.title = 'Login Failed';
+        err.errors = ['The provided credentials were invalid.'];
+        return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({ user });
+}));
+
 // Logout: DELETE /api/session
+
+
 // Get session user: GET /api/session
 
 
