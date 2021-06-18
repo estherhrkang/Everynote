35d98abe264dd33a945f9dbad16b6a36004a72a5
Hye Ri Kang <estherhrkang@gmail.com>
2021-06-18 12:05:34 -0400

    set up profile dropdown button

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,7 +1,47 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import { logout} from '../../store/session';
 
-const ProfileButton = () => {
+const ProfileButton = ({ user }) => {
+    const dispatch = useDispatch();
+
+    const [showMenu, setShowMenu] = useState(false);
+    
+    useEffect(() => {
+        if (!showMenu) return;
+        
+        const closeMenu = () => setShowMenu(false);
+        
+        document.addEventListener('click', closeMenu);
+        
+        return () => document.removeEventListener('click', closeMenu);
+    }, [showMenu]);
+
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+    };
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        dispatch(logout());
+    };
+
     return (
-        <i className='fab fa-evernote'></i>
+        <>
+            <button onClick={openMenu}>
+                <i className='fab fa-evernote' />
+            </button>
+            {showMenu && (
+                <ul>
+                    <li>{user.username}</li>
+                    <li>{user.email}</li>
+                    <li>
+                        <button onClick={handleLogout}>Sign Out</button>
+                    </li>
+                </ul>
+            )}
+        </>
     );
 };
 
