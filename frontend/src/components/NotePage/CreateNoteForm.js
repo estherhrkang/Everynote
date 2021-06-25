c25a07f2677090611c605e60820ede22afd01899
Hye Ri Kang <estherhrkang@gmail.com>
2021-06-25 17:40:22 -0400

    set up creating separate component for create and edit note

---
+++ frontend/src/components/NotePage/CreateNoteForm.js
@@ -0,0 +1,8 @@
+const CreateNoteForm = () => {
+
+    return (
+        <h3>create note form</h3>
+    );
+};
+
+export default CreateNoteForm;
---
+++ frontend/src/components/NotePage/EditNoteForm.js
@@ -0,0 +1,8 @@
+const EditNoteForm = () => {
+
+    return (
+        <h3>edit note form</h3>
+    );
+};
+
+export default EditNoteForm;
